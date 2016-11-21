6a0139a1fbe7abb67f1040683aa1d188a386ae1d
James Hensman <james.hensman@gmail.com>
2016-11-21 14:57:48 +0000

    adding simple VFF/RFF figure

---
+++ experiments/simple_regression.py/figure.py
@@ -0,0 +1,59 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import GPflow
+import VFF
+plt.ion()
+# import matplotlib2tikz
+plt.close('all')
+
+X = np.vstack([np.random.rand(10, 1), np.random.rand(10, 1)*0.5])
+Y = np.sin(3*X) + 0.4*np.cos(9*X) + 0.1 * np.random.randn(*X.shape)
+
+# whether or not to optimize the parameters of the models. If False, use optimial parameters from ful model.
+optimize = False
+
+
+def plot(m, ax=None):
+    if ax is None:
+        f, ax = plt.subplots(1, 1)
+    xtest = np.linspace(-0.2, 1.2, 100)[:, None]
+    mu, var = m.predict_y(xtest)
+    line, = ax.plot(xtest, mu, lw=1.5)
+    ax.plot(xtest, mu + 2*np.sqrt(var), color=line.get_color())
+    ax.plot(xtest, mu - 2*np.sqrt(var), color=line.get_color())
+    ax.plot(m.X.value, m.Y.value, 'kx', mew=1.5)
+
+
+# build a full model to get hypers.
+K = GPflow.kernels.Matern12
+m_full = GPflow.gpr.GPR(X, Y, kern=K(1))
+m_full.optimize()
+
+f, axes = plt.subplots(2, 3, sharex=True, sharey=True)
+axes = axes.flatten()
+ax_count = 0
+for M in [20, 100, 500]:
+    m = VFF.SSGP(X, Y, kern=K(1), num_basis=M)
+    m.omega.fixed = True
+    m.kern.set_parameter_dict(m_full.kern.get_parameter_dict())
+    m.likelihood.set_parameter_dict(m_full.likelihood.get_parameter_dict())
+    if optimize:
+        m.optimize()
+    plot(m, axes[ax_count])
+    axes[ax_count].set_title('RFF (%i)' % M)
+    ax_count += 1
+
+for M in [20, 100]:
+    m = VFF.gpr.GPR_1d(X, Y, np.arange(M), a=-1, b=2, kern=K(1))
+    m.kern.set_parameter_dict(m_full.kern.get_parameter_dict())
+    m.likelihood.set_parameter_dict(m_full.likelihood.get_parameter_dict())
+    if optimize:
+        m.optimize()
+    plot(m, axes[ax_count])
+    axes[ax_count].set_title('VFF (%i)' % M)
+    ax_count += 1
+
+plot(m_full, axes[ax_count])
+axes[ax_count].set_title('Full')
+axes[ax_count].set_xlim(-0.1, 1.2)
+axes[ax_count].set_ylim(-0.7, 1.7)
