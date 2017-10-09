94ce4c075e8ba138c95f34caf6c05cf4f48b9500
Nicolas Durrande <nicolas@prowler.io>
2017-10-09 19:00:22 +0100

    progress in the experiment

--- experiments/increasing_dim/Exp_1/kron.py
+++ experiments/increasing_dim/Exp_1/kron.py
@@ -1,11 +1,10 @@
 import GPflow
 import numpy as np
 from time import time
-from sklearn.cluster import KMeans
 import pandas as pd
 
-from config import dimensions, noise_var, prodkern, repeats, num_inducing
-
+from config import dimensions, noise_var, prodkern, repeats, num_freqs
+import VFF
 
 results = pd.DataFrame()
 
@@ -13,13 +12,23 @@ for dim in dimensions:
     for r in range(repeats):
         print('replicate ',r,'/',repeats)
         data = np.load('data/data_dim{}_rep{}.npz'.format(dim, r))
-        for M in num_inducing:
-            # build sparse GP model
-            Z = KMeans(n_clusters=M).fit(data['Xtrain']).cluster_centers_
-            m = GPflow.sgpr.SGPR(data['Xtrain'], data['Ytrain'], Z=Z, kern=prodkern(dim))
+        for M in num_freqs:
+            if (M ** dim) > 1e5:
+                continue
+            # build kronecker GP model
+            a, b = -0.5 * np.ones(dim), 1.5 * np.ones(dim)
+            m = VFF.vgp.VGP_kron(data['Xtrain'], data['Ytrain'], np.arange(M), a, b,
+                                 kerns=prodkern(dim).kern_list,
+                                 likelihood=GPflow.likelihoods.Gaussian(),
+                                 use_two_krons=True)
             m.likelihood.variance = noise_var
 
+            # only optimize q(u)
+            m.kerns.fixed = True
+            m.likelihood.fixed = True
+
             start = time()
+            m.optimize()
             marg_lik = m.compute_log_likelihood().squeeze()
             mean_log_pred = np.mean(m.predict_density(data['Xtest'], data['Ytest']))
             t = time() - start
@@ -30,5 +39,4 @@ for dim in dimensions:
                                      ignore_index=True)
 
             # do this inside the loop so we can get partial results if something crashes
-            results.to_csv('results/sparse_kmeans.csv')
-
+            results.to_csv('results/kron_opt.csv')
---
+++ experiments/increasing_dim/Exp_1/run_experiment.py
@@ -0,0 +1,1 @@
+import gen_data
