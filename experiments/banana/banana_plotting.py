786d4c19078ec39a2e9ed88038e0112fa6a71fb4
ST John <st--@users.noreply.github.com>
2021-03-10 17:06:29 +0000

    fix banana_plotting script

--- experiments/banana/banana_plotting.py
+++ experiments/banana/banana_plotting.py
@@ -16,7 +16,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.cluster.vq import kmeans
-import vff
+from VFF.vgp import VGP_kron
 import gpflow
 
 np.random.seed(0)
@@ -36,11 +36,11 @@ def plot(m, ax):
         fig, ax = plt.subplots(1, 1, figsize=(6, 6))
     xtest, ytest = np.mgrid[-2.5:2.5:100j, -2.5:2.5:100j]
     Xtest = np.vstack((xtest.flatten(), ytest.flatten())).T
-    for i, mark in [[1, "x"], [2, "o"]]:
-        ind = m.Y.value[:, 0] == i
-        ax.plot(m.X.value[ind, 0], m.X.value[ind, 1], mark)
+    for i, mark in [[0, "x"], [1, "o"]]:
+        ind = Y[:, 0] == i
+        ax.plot(X[ind, 0], X[ind, 1], mark)
     mu, var = m.predict_y(Xtest)
-    ax.contour(xtest, ytest, mu.reshape(100, 100), levels=[0.5], colors="C0", linewidths=4)
+    ax.contour(xtest, ytest, mu.numpy().reshape(100, 100), levels=[0.5], colors="C0", linewidths=4)
 
     ax.set_xlim(-2.5, 2.5)
     ax.set_ylim(-2.5, 2.5)
@@ -48,40 +48,48 @@ def plot(m, ax):
 
 # Variational Fourier Features
 models = []
+labels = []
 for M in [2, 4, 8, 16]:
-    m = vff.vgp.VGP_kron(
-        X, Y, np.arange(M), a=a, b=b, kerns=[k(1), k(1)], likelihood=lik(), use_two_krons=True
+    m = VGP_kron(
+        (X, Y), np.arange(M), a=a, b=b, kernels=[k(), k()], likelihood=lik(), use_two_krons=True
     )
     models.append(m)
+    labels.append(f"VFF {M}")
 
 # Pseudo-inputs
 for M in [4, 8, 16, 32]:
-    kern = k(1, active_dims=[0]) * k(1, active_dims=[1])
+    kern = k(active_dims=[0]) * k(active_dims=[1])
     Z, _ = kmeans(X, M)
-    m = gpflow.models.SVGP(X, Y, kern=kern, likelihood=lik(), Z=Z)
+    m = gpflow.models.SVGP(kernel=kern, likelihood=lik(), inducing_variable=Z)
     models.append(m)
+    labels.append(f"IIP {M}")
 
 # full
-m = gpflow.models.VGP(X, Y, kern=k(1, active_dims=[0]) * k(1, active_dims=[1]), likelihood=lik())
+m = gpflow.models.VGP((X, Y), kernel=k(active_dims=[0]) * k(active_dims=[1]), likelihood=lik())
 models.append(m)
+labels.append("Full")
 
 ###############
-for m in models:
+for label, m in zip(labels, models):
+    print(f"Optimizing {name}")
     try:
-        o = gpflow.train.ScipyOptimizer()
-        o.minimize(m)
+        o = gpflow.optimizers.Scipy()
+        if isinstance(m, gpflow.models.ExternalDataTrainingLossMixin):
+            loss = m.training_loss_closure((X, Y))
+        else:
+            loss = m.training_loss
+        o.minimize(loss, m.trainable_variables)
     except:
         print("model optimization failed")
 
 ###############
-labels = ["VFF 2", "VFF 4", "VFF 8", "VFF 16", "IIP 4", "IIP 8", "IIP 16", "IIP 32", "Full"]
 f, axes = plt.subplots(3, 3, sharex=True, sharey=True, figsize=(15, 15))
 axes = axes.flatten()
-for ax, lab, m in zip(axes, labels, models):
+for ax, label, m in zip(axes, labels, models):
     plot(m, ax)
-    ax.set_title(lab)
-    if hasattr(m, "Z"):
-        ax.plot(m.Z.value[:, 0], m.Z.value[:, 1], "ko", ms=4)
+    ax.set_title(label)
+    if isinstance(m, gpflow.models.SVGP):
+        ax.plot(m.inducing_variable.Z[:, 0], m.inducing_variable.Z[:, 1], "ko", ms=4)
 
 
 # m2t.save('banana_compare.tikz')
