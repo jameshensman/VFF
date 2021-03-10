11303478bc8855b0272b0103dd2b7477ee7c8a9c
ST John <st--@users.noreply.github.com>
2021-03-10 11:22:32 +0000

    convert tests to tf2

--- testing/test_kronecker_ops.py
+++ testing/test_kronecker_ops.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import numpy as np
 from unittest import TestCase
-import kronecker_ops
-from matrix_structures import LowRankMat
+from VFF import kronecker_ops
+from VFF.matrix_structures import LowRankMat
 from functools import reduce
 
 
@@ -13,37 +13,33 @@ class TestKrons(TestCase):
         self.ds_np = [np.random.rand(self.M) + 0.1 for i in range(self.D)]
         self.Ws_np = [np.random.randn(self.M, 2) for i in range(self.D)]
         self.Ks_np = [np.diag(d) + np.dot(W, W.T) for d, W in zip(self.ds_np, self.Ws_np)]
+        self.ds_tf = [tf.convert_to_tensor(d) for d in self.ds_np]
+        self.Ws_tf = [tf.convert_to_tensor(W) for W in self.Ws_np]
+        self.Ks_tf = [LowRankMat(d, W) for d, W in zip(self.ds_tf, self.Ws_tf)]
 
         self.K_np = self.Ks_np[0] * 1
         for ki in self.Ks_np[1:]:
             self.K_np = np.kron(self.K_np, ki)
 
-        self.ds_tf = [tf.placeholder(tf.float64, [self.M]) for i in range(self.D)]
-        self.Ws_tf = [tf.placeholder(tf.float64, [self.M, 2]) for i in range(self.D)]
-        self.Ks_tf = [LowRankMat(d, W) for d, W in zip(self.ds_tf, self.Ws_tf)]
-
-        self.session = tf.Session()
-        self.feed = dict(zip(self.ds_tf + self.Ws_tf, self.ds_np + self.Ws_np))
-
     def test_kron_vec_mul(self):
         v = np.random.randn(self.M ** self.D, 1)
         Ks = [K.get() for K in self.Ks_tf]
-        res_tf = self.session.run(kronecker_ops.kron_vec_mul(Ks, v), self.feed)
+        res_tf = kronecker_ops.kron_vec_mul(Ks, v)
         res_np = np.dot(self.K_np, v)
-        self.assertTrue(np.allclose(res_tf, res_np))
+        np.testing.assert_allclose(res_tf, res_np)
 
     def test_kron_mat_mul(self):
         mat = np.random.randn(self.M ** self.D, 3)
         Ks = [K.get() for K in self.Ks_tf]
-        res_tf = self.session.run(kronecker_ops.kron_mat_mul(Ks, mat, 3), self.feed)
+        res_tf = kronecker_ops.kron_mat_mul(Ks, mat, 3)
         res_np = np.dot(self.K_np, mat)
-        self.assertTrue(np.allclose(res_tf, res_np))
+        np.testing.assert_allclose(res_tf, res_np)
 
     def test_kron_vec_apply_solve(self):
         B = np.random.randn(self.M ** self.D, 1)
         res_np = np.linalg.solve(self.K_np, B)
-        res_tf = self.session.run(kronecker_ops.kron_vec_apply(self.Ks_tf, B, "solve"), self.feed)
-        self.assertTrue(np.allclose(res_tf, res_np))
+        res_tf = kronecker_ops.kron_vec_apply(self.Ks_tf, B, "solve")
+        np.testing.assert_allclose(res_tf, res_np)
 
 
 class TestKVS(TestCase):
@@ -52,6 +48,7 @@ class TestKVS(TestCase):
         self.N = 20
         self.D = 3
         self.Ks_np = [np.random.randn(self.N, self.M) for i in range(self.D)]
+        self.Ks_tf = [tf.convert_to_tensor(K) for K in self.Ks_np]
 
         rows = [
             np.kron(np.kron(self.Ks_np[0][i], self.Ks_np[1][i]), self.Ks_np[2][i])
@@ -59,45 +56,36 @@ class TestKVS(TestCase):
         ]
         self.K_full = np.vstack(rows)
 
-        self.Ks_tf = [tf.placeholder(tf.float64, [self.N, self.M]) for i in range(self.D)]
-
-        self.session = tf.Session()
-        self.feed = dict(zip(self.Ks_tf, self.Ks_np))
-
     def test_make_kvs(self):
-        tf_res = self.session.run(kronecker_ops.make_kvs(self.Ks_tf), self.feed)
-        self.assertTrue(np.allclose(tf_res, self.K_full))
+        tf_res = kronecker_ops.make_kvs(self.Ks_tf)
+        np.testing.assert_allclose(tf_res, self.K_full)
 
     def test_kvs_dot_vec(self):
         B = np.random.randn(self.M ** self.D, 1)
-        tf_res = self.session.run(kronecker_ops.kvs_dot_vec(self.Ks_tf, B), self.feed)
+        tf_res = kronecker_ops.kvs_dot_vec(self.Ks_tf, B)
         np_res = np.dot(self.K_full, B)
-        self.assertTrue(np.allclose(tf_res, np_res))
+        np.testing.assert_allclose(tf_res, np_res)
 
 
-class TestLogDetSum_np(TestCase):
-    def setUp(self):
-        self.Ls = [np.tril(np.random.randn(3, 3)) for i in range(3)]
-        self.Rs = [np.tril(np.random.randn(3, 3)) for i in range(3)]
+def test_logdetsum_np():
+    Ls = [np.tril(np.random.randn(3, 3)) for i in range(3)]
+    Rs = [np.tril(np.random.randn(3, 3)) for i in range(3)]
 
-    def test_hard(self):
-        LLs = [np.dot(L, L.T) for L in self.Ls]
-        RRs = [np.dot(R, R.T) for R in self.Rs]
-        _, result1 = np.linalg.slogdet(reduce(np.kron, LLs) + reduce(np.kron, RRs))
-        self.assertTrue(np.allclose(result1, kronecker_ops.log_det_kron_sum_np(self.Ls, self.Rs)))
+    actual = kronecker_ops.log_det_kron_sum_np(Ls, Rs)
+
+    LLs = [np.dot(L, L.T) for L in Ls]
+    RRs = [np.dot(R, R.T) for R in Rs]
+    _, expected = np.linalg.slogdet(reduce(np.kron, LLs) + reduce(np.kron, RRs))
+
+    np.testing.assert_allclose(actual, expected)
 
 
-class TestLogDetSum_tf(TestCase):
-    def setUp(self):
-        self.Ls_np = [np.tril(np.random.randn(3, 3)) for i in range(3)]
-        self.Rs_np = [np.tril(np.random.randn(3, 3)) for i in range(3)]
-        self.Ls_tf = [tf.placeholder(tf.float64, [None, None]) for i in range(3)]
-        self.Rs_tf = [tf.placeholder(tf.float64, [None, None]) for i in range(3)]
+def test_logdetsum_tf():
+    Ls_np = [np.tril(np.random.randn(3, 3)) for i in range(3)]
+    Rs_np = [np.tril(np.random.randn(3, 3)) for i in range(3)]
+    Ls_tf = [tf.convert_to_tensor(L) for L in Ls_np]
+    Rs_tf = [tf.convert_to_tensor(R) for R in Rs_np]
 
-        self.feed = dict(zip(self.Ls_tf + self.Rs_tf, self.Ls_np + self.Rs_np))
-        self.session = tf.Session()
-
-    def test_hard(self):
-        res_np = kronecker_ops.log_det_kron_sum_np(self.Ls_np, self.Rs_np)
-        res_tf = self.session.run(kronecker_ops.log_det_kron_sum(self.Ls_tf, self.Rs_tf), self.feed)
-        self.assertTrue(np.allclose(res_np, res_tf))
+    res_np = kronecker_ops.log_det_kron_sum_np(Ls_np, Rs_np)
+    res_tf = kronecker_ops.log_det_kron_sum(Ls_tf, Rs_tf)
+    np.testing.assert_allclose(res_tf, res_np)
--- testing/test_matrix_structures.py
+++ testing/test_matrix_structures.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-from matrix_structures import DiagMat, Rank1Mat, LowRankMat, BlockDiagMat
+from VFF.matrix_structures import DiagMat, Rank1Mat, LowRankMat, BlockDiagMat
 from unittest import TestCase
 
 np.random.seed(0)
@@ -11,47 +11,46 @@ class TestDiagMats(TestCase):
         self.N = 20
         self.d_np = np.random.rand(self.N) + 0.1
         self.A_np = np.diag(self.d_np)
-        self.d_tf = tf.placeholder(tf.float64)
+        self.d_tf = tf.convert_to_tensor(self.d_np)
         self.mat = DiagMat(self.d_tf)
-        self.session = tf.Session()
-        self.feed = {self.d_tf: self.d_np}
 
     def test_get(self):
-        A_tf = self.session.run(self.mat.get(), self.feed)
-        self.assertTrue(np.allclose(A_tf, self.A_np))
+        A_tf = self.mat.get()
+        np.testing.assert_allclose(A_tf, self.A_np)
 
     def test_logdet(self):
-        logdet_tf = self.session.run(self.mat.logdet(), self.feed)
-        self.assertTrue(np.allclose(np.linalg.slogdet(self.A_np)[1], logdet_tf))
+        logdet_tf = self.mat.logdet()
+        _, logdet_np = np.linalg.slogdet(self.A_np)
+        np.testing.assert_allclose(logdet_tf, logdet_np)
 
     def test_matmul(self):
         B = np.random.randn(self.N, self.N + 2)
-        AB_tf = self.session.run(self.mat.matmul(B), self.feed)
-        self.assertTrue(np.allclose(AB_tf, np.dot(self.A_np, B)))
+        AB_tf = self.mat.matmul(B)
+        np.testing.assert_allclose(AB_tf, np.dot(self.A_np, B))
 
     def test_solve(self):
         B = np.random.randn(self.N, self.N + 2)
-        AiB_tf = self.session.run(self.mat.solve(B), self.feed)
-        self.assertTrue(np.allclose(AiB_tf, np.linalg.solve(self.A_np, B)))
+        AiB_tf = self.mat.solve(B)
+        np.testing.assert_allclose(AiB_tf, np.linalg.solve(self.A_np, B))
 
     def test_trace_KiX(self):
         B = np.random.randn(self.N, self.N)
-        tr_AiB_tf = self.session.run(self.mat.trace_KiX(B), self.feed)
-        self.assertTrue(np.allclose(tr_AiB_tf, np.trace(np.linalg.solve(self.A_np, B))))
+        tr_AiB_tf = self.mat.trace_KiX(B)
+        np.testing.assert_allclose(tr_AiB_tf, np.trace(np.linalg.solve(self.A_np, B)))
 
     def test_trace_KiX_against_solve(self):
         B = np.random.randn(self.N, self.N)
-        tr_AiB_tf = self.session.run(self.mat.trace_KiX(B), self.feed)
-        tr_AiB_tf2 = self.session.run(tf.reduce_sum(tf.diag_part(self.mat.solve(B))), self.feed)
-        self.assertTrue(np.allclose(tr_AiB_tf, tr_AiB_tf2))
+        tr_AiB_tf = self.mat.trace_KiX(B)
+        tr_AiB_tf2 = tf.reduce_sum(tf.linalg.diag_part(self.mat.solve(B)))
+        np.testing.assert_allclose(tr_AiB_tf, tr_AiB_tf2)
 
     def test_get_diag(self):
-        d_tf = self.session.run(self.mat.get_diag(), self.feed)
-        self.assertTrue(np.allclose(np.diag(self.A_np), d_tf))
+        d_tf = self.mat.get_diag()
+        np.testing.assert_allclose(d_tf, np.diag(self.A_np))
 
     def test_inv_diag(self):
-        di_tf = self.session.run(self.mat.inv_diag(), self.feed)
-        self.assertTrue(np.allclose(di_tf, np.diag(np.linalg.inv(self.A_np))))
+        di_tf = self.mat.inv_diag()
+        np.testing.assert_allclose(di_tf, np.diag(np.linalg.inv(self.A_np)))
 
 
 class TestR1Mat(TestDiagMats):
@@ -60,11 +59,9 @@ class TestR1Mat(TestDiagMats):
         self.d_np = np.random.rand(self.N) + 0.1
         self.v_np = np.random.randn(self.N)
         self.A_np = np.diag(self.d_np) + self.v_np[:, None] * self.v_np[None, :]
-        self.d_tf = tf.placeholder(tf.float64)
-        self.v_tf = tf.placeholder(tf.float64)
+        self.d_tf = tf.convert_to_tensor(self.d_np)
+        self.v_tf = tf.convert_to_tensor(self.v_np)
         self.mat = Rank1Mat(self.d_tf, self.v_tf)
-        self.session = tf.Session()
-        self.feed = {self.d_tf: self.d_np, self.v_tf: self.v_np}
 
 
 class TestLRMat(TestDiagMats):
@@ -73,11 +70,9 @@ class TestLRMat(TestDiagMats):
         self.d_np = np.random.rand(self.N) + 0.1
         self.W_np = np.random.randn(self.N, 2)
         self.A_np = np.diag(self.d_np) + np.dot(self.W_np, self.W_np.T)
-        self.d_tf = tf.placeholder(tf.float64)
-        self.W_tf = tf.placeholder(tf.float64)
+        self.d_tf = tf.convert_to_tensor(self.d_np)
+        self.W_tf = tf.convert_to_tensor(self.W_np)
         self.mat = LowRankMat(self.d_tf, self.W_tf)
-        self.session = tf.Session()
-        self.feed = {self.d_tf: self.d_np, self.W_tf: self.W_np}
 
 
 class TestBlockMat(TestDiagMats):
@@ -95,17 +90,10 @@ class TestBlockMat(TestDiagMats):
                 np.hstack([np.zeros([self.N2, self.N1]), self.A2_np]),
             ]
         )
-        self.d1_tf = tf.placeholder(tf.float64)
-        self.W1_tf = tf.placeholder(tf.float64)
-        self.d2_tf = tf.placeholder(tf.float64)
-        self.W2_tf = tf.placeholder(tf.float64)
+        self.d1_tf = tf.convert_to_tensor(self.d1_np)
+        self.W1_tf = tf.convert_to_tensor(self.W1_np)
+        self.d2_tf = tf.convert_to_tensor(self.d2_np)
+        self.W2_tf = tf.convert_to_tensor(self.W2_np)
         self.mat = BlockDiagMat(
             LowRankMat(self.d1_tf, self.W1_tf), LowRankMat(self.d2_tf, self.W2_tf)
         )
-        self.session = tf.Session()
-        self.feed = {
-            self.d1_tf: self.d1_np,
-            self.W1_tf: self.W1_np,
-            self.d2_tf: self.d2_np,
-            self.W2_tf: self.W2_np,
-        }
